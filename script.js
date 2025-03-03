784e3ddd1228c64cb2b4fcb764866c2e7008799b
Olivia Boone <144468291+Olivia9444@users.noreply.github.com>
2025-03-03 10:52:19 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Write your JavaScript below this line
+var numberOfHorses = "2";
+var favoriteFlower = "Hydragenga";
+var cityBorn = "AppleValley";
+var birthday = "September"
+
+console.log( numberOfHorses);
+console.log("My favorite flower 
+is the " + 
+favoriteFlower + ".");
+console.log ("I was born in " + cityBorn
+    + "on " + birthday + ".");
